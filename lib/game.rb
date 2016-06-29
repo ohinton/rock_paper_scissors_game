67393712da854244ec8d444fda831a257ec69594
Olivia Hinton <hinton.olivia@gmail.com>
2016-06-29 13:58:37 -0700

    refactor logic

--- lib/game.rb
+++ lib/game.rb
@@ -2,23 +2,11 @@ require('pry')
 
 class String
   define_method(:game) do |player_two|
-    if self == "rock" && player_two == "scissors"
+    if self == "rock" && player_two == "scissors" || self == "paper" && player_two == "rock" || self == "scissors" && player_two == "paper"
       winner = "Player One!"
-    elsif self == "rock" && player_two == "paper"
+    elsif self == "rock" && player_two == "paper" || self == "paper" && player_two == "scissors" || self == "scissors" && player_two == "rock"
       winner = "Player Two!"
-    elsif self == "rock" && player_two == "rock"
-      winner = "No one! It's a tie!"
-    elsif self == "paper" && player_two == "rock"
-      winner = "Player One!"
-    elsif self == "paper" && player_two == "scissors"
-      winner = "Player Two!"
-    elsif self == "paper" && player_two == "paper"
-      winner = "No one! It's a tie!"
-    elsif self == "scissors" && player_two == "paper"
-      winner = "Player One!"
-    elsif self == "scissors" && player_two == "rock"
-      winner = "Player Two!"
-    else self == "scissors" && player_two == "scissors"
+    else
       winner = "No one! It's a tie!"
     end
     winner
